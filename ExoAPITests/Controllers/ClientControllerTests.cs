a2a6374c562cc3f2f52509bccfb7ca5f62061620
ValentinFurcy <v.furcy@laposte.net>
2024-01-18 11:46:38 +0100

    commit

---
+++ ExoAPITests/Controllers/ClientControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExoAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class ClientControllerTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ ExoAPITests/Repositories/ClientRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoAPITests.Repositories.Tests
+{
+    [TestClass()]
+    public class ClientRepositoryTests
+    {
+        [TestMethod()]
+        public async void GetAllClient()
+        {
+            //Arrange
+            var builder = new DbContextOptionsBuilder<ClientDbContext>().UseInMemoryDatabase("client");
+
+            var context = new ClientDbContext(builder.Options);
+        }
+    }
+}
